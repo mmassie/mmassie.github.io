9a8b0552ea08b31e04577155e9a9972d549f6c30
Mike Massie <michael.a.massie@gmail.com>
2018-12-23 16:15:54 -0600

    supporting files added

---
+++ The_Smart_Alarm_files/The_Smart_Alarm.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120617
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),stroke_1:new IWPhotoFrame([IWCreateImage('The_Smart_Alarm_files/FormalShadow_01.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_02.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_03.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_06.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_12.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_11.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_10.png'),IWCreateImage('The_Smart_Alarm_files/FormalShadow_04.png')],null,2,0.700000,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('The_Smart_Alarm_files/The_Smart_AlarmMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
---
+++ tuppachair_files/tuppachair.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120207
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('tuppachair_files/GraphPaper_01.png'),IWCreateImage('tuppachair_files/GraphPaper_02.png'),IWCreateImage('tuppachair_files/GraphPaper_01_1.png'),IWCreateImage('tuppachair_files/GraphPaper_01_2.png'),IWCreateImage('tuppachair_files/GraphPaper_09.png'),IWCreateImage('tuppachair_files/GraphPaper_08.png'),IWCreateImage('tuppachair_files/GraphPaper_07.png'),IWCreateImage('tuppachair_files/GraphPaper_01_3.png')],null,0,0.687322,90.000000,0.000000,90.000000,0.000000,89.000000,11.000000,89.000000,7.000000,11.000000,11.000000,11.000000,11.000000,'tuppachair_files/graphpaper_tape.png',new IWPoint(0.500000,0),new IWSize(155,47),0.300000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('tuppachair_files/tuppachairMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
